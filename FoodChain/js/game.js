bda5ca32d8cc91cb0a92242409bb71ea33744ba9
Ankit Tandon <ankit.tandon@yahoo.co.in>
2014-03-16 20:29:04 +0530

    new features

--- FoodChain/js/game.js
+++ FoodChain/js/game.js
@@ -8,6 +8,54 @@ var door_rotating = 0;
 
 var cells_animals =  {"0,0":null,"0,1":null,"0,2":null,"1,0":null,"1,1":null,"1,2":null,"2,0":null,"2,1":null,"2,2":null,"3,0":null,"3,1":null,"3,2":null};
 var fence_coord = {};
+var battery_sheet = null;
+var heart_sheet = null;
+var heart;
+var heart_left=0;
+
+
+function MenuState() {
+
+    var menu;
+    var background;
+
+    this.setup = function() {
+        menu = new jaws.Sprite({image: "img/menu.png", x: 100, y: 200});
+        background = new jaws.Sprite({color: "#8ED6FF", width: jaws.width, height: jaws.height, x: 0, y: 0});
+    };
+    this.update = function() {
+        if (jaws.pressed("enter")) {
+            jaws.switchGameState(PlayState);
+        }
+    };
+    this.draw = function() {
+        background.draw();
+        menu.draw();
+    };
+}
+function EndState() {
+
+    var end;
+    var background;
+
+    this.setup = function() {
+        end = new jaws.Sprite({image: "img/end.png", x: 100, y: 200});
+        background = new jaws.Sprite({color: "#8ED6FF", width: jaws.width, height: jaws.height, x: 0, y: 0});
+    };
+    this.update = function() {
+        if (jaws.pressed("enter")) {
+            jaws.switchGameState(PlayState);
+        }
+    };
+    this.draw = function() {
+        background.draw();
+        end.draw();
+    };
+}
+
+
+
+
 
 function PlayState() {
 
@@ -15,13 +63,21 @@ function PlayState() {
 
     var maxHeight = 600;
 
-    var tiger1;
-    var tiger2;
+    var tiger;
+    var rabbit;
+    var deer;
+    var croc;
+    var zebra;
+    var lion;
+
     var wall;
     var fence_back;
     var fence_gate;
     var rotating_fence;
 
+
+    battery_sheet = new jaws.SpriteSheet({image: "img/battery.png", frame_size: [39, 18], orientation: "right"});
+    heart_sheet = new jaws.SpriteSheet({image: "img/heartSheet.png", frame_size: [78, 64], orientation: "right"});
 
     var sea;
     this.draw_fence = function(mode,row,column) {
@@ -74,10 +130,15 @@ function PlayState() {
 
         //Player properties  
         //tiger = new jaws.Sprite({image: "images/squid.png", x: 30, y: 30});
-        tiger1 = new Animal (0,1,"images/squid.png");
-
-        tiger2 = new Animal (2,2,"images/squid.png");
-
+        tiger = new Animal (0,1,"img/tiger_left.png","img/tiger_right.png",1,58,30,16,100);
+        rabbit = new Animal (1,1,"img/rabbit_left.png","img/rabbit_right.png",1,40,40,10,100);
+        deer = new Animal (2,1,"img/deer_left.png","img/deer_right.png",1,60,49,7,100);
+        croc = new Animal (2,2,"img/croc_left.png","img/croc_right.png",0,93,51,4,800);
+        zebra = new Animal (2,3,"img/zebra_left.png","img/zebra_right.png",1,54,54,9,300);
+        lion = new Animal (1,3,"img/lion_left.png","img/lion_right.png",1,80,61,12,300);
+        heart = new jaws.Sprite({ x: 515, y: 05});
+       // battery_sheet = new jaws.SpriteSheet({image: "img/battery.png", frame_size: [39, 54], orientation: "right"});
+       // console.log("Total elements : " + battery_sheet.frames.length);
 
         //Coins properties
         wall = new jaws.SpriteList();
@@ -117,11 +178,7 @@ function PlayState() {
 
         wall.check = function() {
 
-            //var results = jaws.collideOneWithMany(tiger, wall);
 
-            //if (results.length > 0) {
-               // wall.remove(results[0]);
-            //}
         };
 
         //Sea properties
@@ -131,6 +188,10 @@ function PlayState() {
 
     };
     this.update = function() {
+        if (heart_left == 3) {
+            jaws.switchGameState(EndState);
+        } else {
+        heart.setImage(heart_sheet.frames[heart_left]);
         if (door_rotating == 1) {
             var o = rotating_fence[0];
             //console.log("Found new fence:" + o);
@@ -183,48 +244,112 @@ function PlayState() {
         }
 
 
-        tiger1.move();
-        tiger2.move();
+        tiger.move();
+        rabbit.move();
+        deer.move();
+        croc.move();
+        zebra.move();
+        lion.move();
+
         wall.check();
 
-    };
+    }};
     this.draw = function() {
         jaws.clear();
         viewport.draw(sea);
         viewport.draw(wall);
         viewport.draw(fence_back);
         viewport.draw(fence_gate);
-        viewport.draw(tiger1.actor);
-        viewport.draw(tiger2.actor);
+        viewport.draw(heart);
+        if (tiger.active == 1) {
+          viewport.draw(tiger.actor);
+          viewport.draw(tiger.health);
+        }
+        if (rabbit.active == 1) {
+        viewport.draw(rabbit.actor);
+        viewport.draw(rabbit.health);
+        }
+        if(deer.active) {
+        viewport.draw(deer.actor);
+        viewport.draw(deer.health);
+        }
+        if (croc.active ==1) {
+        viewport.draw(croc.actor);
+        viewport.draw(croc.health);
+        }
+        if (zebra.active == 1) {
+        viewport.draw(zebra.actor);
+        viewport.draw(zebra.health);
+        }
+        if (lion.active == 1) {
+        viewport.draw(lion.actor);
+        viewport.draw(lion.health);
+        }
+
 
     };
 }
 jaws.onload = function() {
-    jaws.assets.add(["images/coin.png", "images/squid.png","img/grass.jpg","img/wall.png","img/wood.png"]);
+    jaws.assets.add(["images/coin.png", "images/squid.png","img/grass.jpg","img/wall.png","img/wood.png","img/tiger_left.png","img/tiger_right.png",
+        "img/rabbit_left.png","img/rabbit_right.png","img/deer_left.png","img/deer_right.png","img/croc_left.png","img/croc_right.png","img/zebra_left.png",
+        "img/zebra_right.png","img/lion_right.png","img/lion_left.png","img/battery.png","img/heartSheet.png","img/menu.png","img/end.png"]);
     jaws.start(PlayState);
 };
 
 
- function Animal(row,column,asset) {
+
+
+
+ function Animal(row,column,asset_left,asset_right,speed,frame_width,frame_height,frames,fd) {
 
     this.cellrow =  row;
     this.cellcolumn = column;
+    this.energy  = 100*Math.random();
+    this.spend = 0.01;
+    this.active = 1;
     var s_x = row * sw_h * 1.5;
     var s_y = column * sw_h;
     var size_x = sw_h * 1.5;
     var size_y = sw_h;
     var move_x = 0;
     var move_y = 0;
+    var speed = speed;
     cells_animals[row + "," + column] = this;
 
-    this.actor = new jaws.Sprite({image: asset, x: s_x + start_x + size_x/2, y: s_y + start_y + size_y/2});
 
+    //console.log("Checking element 0 :" + battery_sheet.frames[0]);
+    this.actor = new jaws.Sprite({x: s_x + start_x + size_x/2, y: s_y + start_y + size_y/2});
+    this.health = new jaws.Sprite({image: battery_sheet.frames[0], x: this.actor.x+20, y: this.actor.y-20});
+     var playerAnim_left = new jaws.Animation({sprite_sheet: asset_left, frame_size: [frame_width,frame_height], frame_duration: fd, orientation: "right"});
+     var playerAnim_right = new jaws.Animation({sprite_sheet: asset_right, frame_size: [frame_width,frame_height], frame_duration: fd, orientation: "right"});
+
+     this.actor.anim_up = playerAnim_left.slice(0, frames -1);
+     this.actor.anim_default = playerAnim_right.slice(0, frames -1);
     this.actor.speed = 1;
-    this.actor.dx = 0;
-    this.actor.dy = 0;
+    this.actor.dx = 0.3;
+    var rand = Math.random();
+    if (rand > 0.5) {
+        this.actor.dx = -1 * this.actor.dx;
+    }
+    this.actor.dy = 0.05;
 
     this.move = function () {
-        this.actor.move();
+        if (this.active == 1) {
+        this.energy = this.energy - this.spend;
+        if (this.energy >= 0) {
+           this.health.setImage(battery_sheet.frames[Math.floor(this.energy/10)]);
+           this.actor.move();
+           this.health.x = this.actor.x + 20;
+           this.health.y = this.actor.y - 20;
+        } else {
+            console.log("Someone killed");
+            this.active = 0;
+            heart_left++;
+            if (heart_left == 3) {
+                jaws.switchGameState(EndState);
+            }
+        }
+        }
     };
 
     this.shift = function(row,column) {
@@ -236,6 +361,7 @@ jaws.onload = function() {
         s_y = column * sw_h;
         move_x = -1 *(this.actor.x - (s_x + start_x + size_x/2));
         move_y = -1 *(this.actor.y - (s_y + start_y + size_y/2));
+
         //console.log("DEBUG: row:" + row + " column:" + column + " s_x:" + s_x + " s_y:" + s_y + " start_x:" + start_x + " start_y:" + start_y + " x:" + this.actor.x + " y:"+ this.actor.y + " size_x:" + size_x + " size_y:" + size_y);
         //console.log("Asked to move:" + this.x - (s_x + start_x + size_x/2) + "," + this.y - (s_y + start_y + size_y/2));
     }
@@ -243,20 +369,24 @@ jaws.onload = function() {
     this.actor.move = function() {
 
        if (move_x == 0 && move_y ==0) {
-        var r1 = Math.random();var r2 = Math.random();var x = 3;var y = 1;
-        if (r1 > 0.8) {this.dx = x;} else if (r1<0.2) {this.dx = -x;}
-        if (r2 > 0.8) {this.dy = y;} else if(r2 < 0.2){this.dy = -y;}
-        this.x += this.dx;this.y += this.dy;
-        if (this.x < (s_x + sw_w + start_x)) {this.x = s_x + sw_w + start_x;}
-        if (this.x > (s_x - sw_w + start_x + size_x)) {this.x = s_x - sw_w + start_x+size_x;}
-        if (this.y < (s_y + sw_w + start_y)) {this.y = s_y + sw_w + start_y;}
-        if (this.y > (s_y - sw_w + start_y + size_y)) {this.y = s_y - sw_w + start_y + size_y;}
-        this.dx = 0;this.dy = 0;
+           if (this.dx < 0)
+           this.setImage(this.anim_up.next());
+           else
+            this.setImage(this.anim_default.next());
+
+        this.x += this.dx*speed;this.y += this.dy*speed;
+        if (this.x < (s_x + sw_w + start_x)) {this.x = s_x + sw_w + start_x;this.dx = this.dx * -1;}
+        if (this.x > (s_x - 3*sw_w + start_x + size_x)) {this.x = s_x - 3*sw_w + start_x+size_x;this.dx = this.dx * -1;}
+        if (this.y < (s_y + sw_w + start_y)) {this.y = s_y + sw_w + start_y;this.dy = this.dy * -1;}
+        if (this.y > (s_y - 3*sw_w + start_y + size_y)) {this.y = s_y - 3*sw_w + start_y + size_y;this.dy = this.dy * -1;}
+        //this.dx = 0;this.dy = 0;
        } else {
            if (move_x > 0) {
+               this.setImage(this.anim_default.next());
                move_x--;
                this.x++;
            } else if (move_x < 0){
+               this.setImage(this.anim_up.next());
                move_x++;
                this.x--;
            }
